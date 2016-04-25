8d4fa1ccb4ddc45ede4a4591d01dcff196bf2935
Craig Parsons <craig@sycle.net>
2016-04-24 17:04:23 -0700

    Initial Commit
    This should start up the github page nicely

---
+++ js/resources/referenceLinks.js
@@ -0,0 +1,51 @@
+var reference_content = [
+  [  // row
+    { // block
+      title: "Materialize Css",
+      link: "http://materializecss.com/",
+      imageURL: "assets/blocks/MaterializeCss.jpeg",
+      keywords: ["Material Design", "CSS", "JQuery", "Responsive front-end framework"]
+    },
+    { // block
+      title: "Laravel",
+      link: "https://laravel.com",
+      imageURL: "https://media.simplecast.com/episode/image/32762/thumb_1458581716-artwork.jpg",
+      keywords: ["PHP back-end framework"],
+      style: {
+        backgroundColor: "#e6e6e6",
+        color: "#777",
+        fontSize: "40px",
+        letterSpacing: "0.5px"
+      }
+    },
+    { // block
+      title: "Wordpress",
+      link: "https://wordpress.org/",
+      imageURL: "assets/blocks/wordpress-logo.jpg",
+      keywords: ["Mysql", "File Upload", "Admin"]
+    }
+  ]
+]
+
+var reference_styles = {
+  squareMode: true,
+  innerBlock: {
+    fontFamily: "Montserrat",
+    color: "#fff",
+    padding: "10px"
+  },
+  blockTitle: {
+    margin: "0 auto",
+    textTransform: "uppercase"
+  },
+  keyword: {
+    fontSize: "11px",
+    backgroundColor: "rgba(0,0,0,0.7)",
+    padding: "2px 5px",
+    margin: "2px"
+  },
+  keywords: {
+    fontSize: "10px",
+    marginTop: "-50px"
+  }
+}
